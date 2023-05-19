60add6f19b5c4064e06017d934477ac1b515f134
Henderson gomes da silva <hendersonkb765@outlook.com.br>
2023-05-19 10:58:44 -0300

    Finalizado

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let goku = false;
+let kala = false;
+let namor = false;
 function clicado(){ 
     
     let btncontexto = event.target
@@ -6,7 +9,7 @@ function clicado(){
        ){
             btncontexto.classList.add("certo")
     }
-    else if(btncontexto.classList[0] == "l7" || btncontexto.classList[0] == "l8"
+    else if(btncontexto.classList[0] =="l6" || btncontexto.classList[0] == "l7" || btncontexto.classList[0] == "l8"
         || btncontexto.classList[0]== "l9" || btncontexto.classList[0]=="l10"){
             btncontexto.classList.add("certo")
     }
@@ -21,6 +24,8 @@ function clicado(){
 }
 
 function verificarAcertos(){
+    var acertos = document.querySelector(".container")
+ 
     //GOKU
     let ag = document.querySelector(".l4")
     let ao = document.querySelector(".l9")
@@ -29,6 +34,7 @@ function verificarAcertos(){
 
     
     //AMOR
+    let bn = document.querySelector(".l6")
     let ba = document.querySelector(".l7")
     let bm = document.querySelector(".l8")
     let bo = document.querySelector(".l9")
@@ -44,20 +50,36 @@ function verificarAcertos(){
     if(ag.classList[1] == "certo"
     && ao.classList[1] == "certo" 
     && ak.classList[1] == "certo" 
-    && au.classList[1] == "certo"){
-        alert("Goku")
+    && au.classList[1] == "certo"
+    && goku == false){
+        goku = true
+        acertos.innerHTML += '<div class="acerto">'+
+        '<img src="resources/image/goku.png" alt="">'+
+        '<h1>GOKU</h1>'+
+        '</div>'
     }
-    else if(ba.classList[1] == "certo" 
+    else if(bn.classList[1] == "certo"
+    &&ba.classList[1] == "certo" 
     && bm.classList[1] =="certo"
      && bo.classList[1] == "certo" 
-     && br.classList[1] == "certo"){
-        alert("Amor")
+     && br.classList[1] == "certo"
+     && namor == false){
+        namor = true
+        acertos.innerHTML += '<div class="acerto">'+
+        '<img src="resources/image/namor.jpg" alt="">'+
+        '<h1>NAMOR</h1>'+
+        '</div>'
     }
     else if(ck.classList[1] == "certo" 
     && ca.classList[1] == "certo"
      && cl.classList[1] == "certo" 
-     && ca1.classList[1] == "certo"){
-        alert("Kala")
+     && ca1.classList[1] == "certo"
+     && kala == false){
+        kala = true
+        acertos.innerHTML += '<div class="acerto">'+
+        '<img src="resources/image/kala.webp" alt="">'+
+        '<h1>KALA</h1>'+
+        '</div>'
     }
 
 }
